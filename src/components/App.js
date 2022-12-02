98a69399fad2e0286d4d4adf5715455eb7362c8b
Fe195 <114395573+Fe195@users.noreply.github.com>
2022-12-02 19:55:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import '../styles/App.css';
+
 const App = () => {
-  return (
-    <p>I am learning React. My life is getting better</p>
+return (
+    <div>
+        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </div>
   )
 }
 
 export default App;
-
